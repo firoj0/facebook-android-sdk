3fbed48cb9af8a71364113bb7316f0a947eb512c
firoj0 <80910955+firoj0@users.noreply.github.com>
2025-01-06 02:53:33 +0530

    Update FacebookAppLinkResolver.java

--- facebook-applinks/src/main/java/com/facebook/applinks/FacebookAppLinkResolver.java
+++ facebook-applinks/src/main/java/com/facebook/applinks/FacebookAppLinkResolver.java
@@ -1,10 +1,10 @@
-/*
+/*firoj Qureshi 
  * Copyright (c) Meta Platforms, Inc. and affiliates.
  * All rights reserved.
  *
  * This source code is licensed under the license found in the
  * LICENSE file in the root directory of this source tree.
- */
+ */p
 
 package com.facebook.applinks;
 
